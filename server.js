54a323899e9b11cc4a7183923184e2150f4df0a9
azri <mukhrizbusiness@gmail.com>
2026-05-15 00:30:14 +0800

    Initial commit with CI/CD workflow

---
+++ server.js
@@ -0,0 +1,21 @@
+// A simple Node.js server that responds with "Hello MERN zrmkhrz" to any request.
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Open http://127.0.0.1:3000/
+app.get('/', (req, res) => {
+    res.send('Hello MERN zrmkhrz with Express!');
+});
+
+// Open http://127.0.0.1:3000/api
+app.get('/api', (req, res) => {
+    res.send('Hello MERN zrmkhrz batch 3!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+});
